a4c20715cac96a7be6c6258c4c9b6eb0812ba069
niodtn <ipegte93@gmail.com>
2025-02-26 00:36:50 +0900

    feat: Add main script to print path and greeting message

---
+++ main.py
@@ -0,0 +1,16 @@
+import sys
+
+
+def main():
+    print("Hello from shot!")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("python main.py <path>")
+        sys.exit(1)
+
+    path = sys.argv[1]
+    print(f"{path}")
+
+    main()
